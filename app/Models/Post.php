2e89971283d5b0577ed151fcb453585949e34001
muhammad.naveed@brainxtech.com <muhammad.naveed@brainxtech.com>
2022-04-27 21:09:08 +0500

    lesson 13 cached the data forever

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,24 +25,30 @@ class Post
 
 
     public static function all(){
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(function ($file){
-            $doc = YamlFrontMatter::parseFile($file);
-            return new Post(
-                $doc->title,
-                $doc->excerpt,
-                $doc->slug,
-                $doc->date,
-                $doc->body()
+//        "Now the cache is not going to clear own its own we have to do it manually"
+        // php artisan tinker to run shell for php and laravel app as a whole
+        // to see cache in tinker type cache('posts.all') in shell
+        //cache()->forget('posts.all') to clear a cash from terminal
+        //cache()->get('key'), cache()->put('key', 'value')
+        return cache()->rememberForever('posts.all', function (){
+            $files = File::files(resource_path("posts/"));
+            $posts_arr = array_map(function ($file){
+                $doc = YamlFrontMatter::parseFile($file);
+                return new Post(
+                    $doc->title,
+                    $doc->excerpt,
+                    $doc->slug,
+                    $doc->date,
+                    $doc->body()
                 );
             },$files);
+            return collect($posts_arr)->sortByDesc('date');
+        });
     }
 
 
     public static function find($slug){
-        $posts = collect(static :: all());
-        return $posts->firstWhere('slug', $slug);
+        return static :: all()->firstWhere('slug', $slug);
     }
 
 
